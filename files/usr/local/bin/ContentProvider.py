173efe373cfb71880f617aca9f82d0aff4db6e0a
Bernhard Bablok <bablokb@gmx.de>
2021-02-19 15:51:05 +0100

    added (empty) create_fonts(), fixed handling of opts

--- files/usr/local/bin/ContentProvider.py
+++ files/usr/local/bin/ContentProvider.py
@@ -23,23 +23,33 @@ class ContentProvider(object):
   def __init__(self,screen):
     """ save screen object """
     self.screen = screen
-    self.opts   = screen._opts
 
   # --- read default settings for content-provider   -------------------------
 
-  def read_settings(self):
+  def read_settings(self,opts):
     """ read settings """
 
     config_file = ContentProvider.CONFIG_FILE_DEFAULT.format(
                                                       self.__class__.__name__)
     if os.path.exists(config_file):
       with open(config_file,"r") as f:
-        self.opts.update(json.load(f))
+        opts.update(json.load(f))
 
   # --- set canvas-property   ------------------------------------------------
 
   def set_canvas(self,canvas):
     self.canvas = canvas
+
+  # --- set opts-property   --------------------------------------------------
+
+  def set_options(self,opts):
+    self.opts = opts
+
+  # --- create fonts   -----------------------------------------------------
+
+  def create_fonts(self):
+    """ create fonts """
+    pass
 
   # --- draw content on screen   ---------------------------------------------
 
