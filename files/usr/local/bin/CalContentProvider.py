04d7ffe3746f20b9c15f037e0d9575a5ece4fdee
Bernhard Bablok <bablokb@gmx.de>
2021-02-19 11:19:55 +0100

    refactoring: use opts/canvas from object

--- files/usr/local/bin/CalContentProvider.py
+++ files/usr/local/bin/CalContentProvider.py
@@ -32,9 +32,9 @@ class CalContentProvider(ContentProvider):
   def get_max_entries(self):
     """ return entries - must be called after dray_day """
 
-    return int((self.screen._opts.HEIGHT-
-                self.screen._opts.HEIGHT_S-self.screen._y_off)/
-                                                    self.screen._opts.HEIGHT_E)
+    return int((self.opts.HEIGHT-
+                self.opts.HEIGHT_S-self.screen._y_off)/
+                                                    self.opts.HEIGHT_E)
 
   # --- read agendas from caldav-servers   ------------------------------------
 
@@ -42,7 +42,7 @@ class CalContentProvider(ContentProvider):
     """ read agenda for all configured calendars """
 
     entries = []
-    for cal_info in self.screen._opts.cals:
+    for cal_info in self.opts.cals:
       self._get_agenda_for_cal(cal_info,entries)
     entries.sort(key=itemgetter(0))
     return entries
@@ -128,40 +128,40 @@ class CalContentProvider(ContentProvider):
 
     # background
     background = [(0,self.screen._y_off+1),
-                    (self.screen._opts.WIDTH,
-                                    self.screen._y_off+self.screen._opts.HEIGHT_E-1)]
-    self.screen._canvas.rectangle(background,fill=e_color)
+                    (self.opts.WIDTH,
+                                    self.screen._y_off+self.opts.HEIGHT_E-1)]
+    self.canvas.rectangle(background,fill=e_color)
   
     # time-value
     tm      = e_time.split('-')
     tm_size = [0,0]
-    tm_size[0] = self.screen._canvas.textsize(tm[0],self.screen._time_font,spacing=0)
-    tm_size[1] = self.screen._canvas.textsize(tm[1],self.screen._time_font,spacing=0)
+    tm_size[0] = self.canvas.textsize(tm[0],self.screen._time_font,spacing=0)
+    tm_size[1] = self.canvas.textsize(tm[1],self.screen._time_font,spacing=0)
     if e_time != "00:00-23:59":
       # only print time for none full-day events
-      self.screen._canvas.text((self.screen._opts.MARGINS[2],self.screen._y_off+2),
+      self.canvas.text((self.opts.MARGINS[2],self.screen._y_off+2),
                         tm[0],font=self.screen._time_font,
                         fill=self.screen._bg_map[e_color])
-      self.screen._canvas.text((self.screen._opts.MARGINS[2],self.screen._y_off+4+tm_size[0][1]),
+      self.canvas.text((self.opts.MARGINS[2],self.screen._y_off+4+tm_size[0][1]),
                         tm[1],font=self.screen._time_font,
                         fill=self.screen._bg_map[e_color])
 
     # text (2 lines)
-    txt_x_off = self.screen._opts.MARGINS[2] + max(tm_size[0][0],tm_size[1][0]) + 4
+    txt_x_off = self.opts.MARGINS[2] + max(tm_size[0][0],tm_size[1][0]) + 4
     txt_y_off = self.screen._y_off + 2
-    text_size = self.screen._canvas.textsize(e_text[0],
+    text_size = self.canvas.textsize(e_text[0],
                                       self.screen._text_font,spacing=0)
-    self.screen._canvas.text((txt_x_off,txt_y_off),e_text[0],
+    self.canvas.text((txt_x_off,txt_y_off),e_text[0],
                       font=self.screen._text_font,
                       fill=self.screen._bg_map[e_color])
 
     txt_y_off += text_size[1]
-    self.screen._canvas.text((txt_x_off,txt_y_off),
+    self.canvas.text((txt_x_off,txt_y_off),
                       e_text[1],font=self.screen._text_font,
                       fill=self.screen._bg_map[e_color])
 
     # ending line
-    self.screen._y_off += self.screen._opts.HEIGHT_E
+    self.screen._y_off += self.opts.HEIGHT_E
     self.screen._draw_hline(self.screen._y_off)
 
   # --- draw content on screen   ---------------------------------------------
