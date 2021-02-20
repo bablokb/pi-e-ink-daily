e7e436eff74fcb9812ab4e69880e8700bb48797a
Bernhard Bablok <bablokb@gmx.de>
2021-02-20 17:09:06 +0100

    new layout: first row also has four tiles

--- files/usr/local/bin/WeatherContentProvider.py
+++ files/usr/local/bin/WeatherContentProvider.py
@@ -6,7 +6,7 @@
 # Content-provider for the weather forecast from OpenWeatherMap
 #
 # This class draws two lines of tiles:
-#   - the first line has three tiles for the current day
+#   - the first line has four tiles for the current day (now and three hours)
 #   - the second line has four tiles for the next four days
 #
 # Author: Bernhard Bablok
@@ -67,9 +67,8 @@ class WeatherContentProvider(ContentProvider):
     # screen-height - current offset - height of status-line
     height = int((self.opts.HEIGHT - self.screen._y_off -
                                             self.opts.HEIGHT_S)/2)
-    width3 = int(self.opts.WIDTH/3)
     width4 = int(self.opts.WIDTH/4)
-    self._sizes = ((width3,height),(width4,height))
+    self._size = ((width4,height))
 
   # --- map weather-id to char of DSEG-font   --------------------------------
 
@@ -94,21 +93,21 @@ class WeatherContentProvider(ContentProvider):
     # hour
     h = hour.dt.strftime("%H:%M")
     h_size = self.canvas.textsize(h,self.screen._text_font,spacing=0)
-    x_plus = int((self._sizes[0][0]-h_size[0])/2)
+    x_plus = int((self._size[0]-h_size[0])/2)
     self.canvas.text((x_off+x_plus,y_off),
                      h,font=self.screen._text_font,
                       fill=self.opts.TEXT_COLOR)
     # temp
     t = "{0:3.1f}°".format(hour.temp)
     t_size = self.canvas.textsize(t,self.screen._text_font,spacing=0)
-    x_plus = int((self._sizes[0][0]-t_size[0])/2)
+    x_plus = int((self._size[0]-t_size[0])/2)
     self.canvas.text((x_off+x_plus,y_off+h_size[1]+1),
                      t,font=self.screen._text_font,
                      fill=self.opts.TEXT_COLOR)
     # icon
     icon = self._map_id(hour.id)
     icon_size = self.canvas.textsize(icon,self._dseg_font,spacing=0)
-    x_plus = int((self._sizes[0][0]-icon_size[0])/2)
+    x_plus = int((self._size[0]-icon_size[0])/2)
     self.canvas.text((x_off+x_plus,y_off+h_size[1]+1+t_size[1]+1),
                      icon,font=self._dseg_font,
                      fill=self.opts.DSEG_COLOR)
@@ -121,21 +120,21 @@ class WeatherContentProvider(ContentProvider):
     # day
     d = day.dt.strftime("%a %d.%m.")
     d_size = self.canvas.textsize(d,self.screen._text_font,spacing=0)
-    x_plus = int((self._sizes[1][0]-d_size[0])/2)
+    x_plus = int((self._size[0]-d_size[0])/2)
     self.canvas.text((x_off+x_plus,y_off),
                      d,font=self.screen._text_font,
                       fill=self.opts.TEXT_COLOR)
     # temp
     t = "{0:d}°/{1:d}°".format(round(day.tmin),round(day.tmax))
     t_size = self.canvas.textsize(t,self.screen._text_font,spacing=0)
-    x_plus = int((self._sizes[1][0]-t_size[0])/2)
+    x_plus = int((self._size[0]-t_size[0])/2)
     self.canvas.text((x_off+x_plus,y_off+d_size[1]+1),
                      t,font=self.screen._text_font,
                      fill=self.opts.TEXT_COLOR)
     # icon
     icon = self._map_id(day.id)
     icon_size = self.canvas.textsize(icon,self._dseg_font,spacing=0)
-    x_plus = int((self._sizes[1][0]-icon_size[0])/2)
+    x_plus = int((self._size[0]-icon_size[0])/2)
     self.canvas.text((x_off+x_plus,y_off+d_size[1]+1+t_size[1]+1),
                      icon,font=self._dseg_font,
                      fill=self.opts.DSEG_COLOR)
@@ -177,10 +176,10 @@ class WeatherContentProvider(ContentProvider):
       h_max = 23
     x_off  = 0
     y_off  = self.screen._y_off
-    height = self._sizes[0][1]
+    height = self._size[1]
     for i in [h_min-h_now,h_mid-h_now,h_max-h_now]:
       self._draw_hour(owm.hours[i],x_off,y_off)
-      x_off += self._sizes[0][0]
+      x_off += self._size[0]
       self.canvas.line([(x_off,y_off),
                                 (x_off,y_off+height)],
                                fill=self.opts.LINE_COLOR,width=1)
@@ -191,10 +190,10 @@ class WeatherContentProvider(ContentProvider):
     # daily forecast
     x_off = 0
     y_off  = self.screen._y_off
-    height = self._sizes[1][1]
+    height = self._size[1]
     for i in range(1,5):
       self._draw_day(owm.days[i],x_off,y_off)
-      x_off += self._sizes[1][0]
+      x_off += self._size[0]
       self.canvas.line([(x_off,y_off),
                               (x_off,y_off+height)],
                              fill=self.opts.LINE_COLOR,width=1)
