ba2d6dc0f77a6750d71aafe40b9ebb8293935748
Bernhard Bablok <bablokb@gmx.de>
2021-02-27 14:57:17 +0100

    support night-icons (part1)

--- files/usr/local/bin/WeatherContentProvider.py
+++ files/usr/local/bin/WeatherContentProvider.py
@@ -25,76 +25,76 @@ from OWMData         import OWMData
 
 class WeatherContentProvider(ContentProvider):
 
-  ID_MAP1 = {
-    200: "\uf01e",   #thunderstorm
-    201: "\uf01e",   #thunderstorm
-    202: "\uf01e",   #thunderstorm
-    210: "\uf016",   #lightning
-    211: "\uf016",   #lightning
-    212: "\uf016",   #lightning
-    221: "\uf016",   #lightning
-    230: "\uf01e",   #thunderstorm
-    231: "\uf01e",   #thunderstorm
-    232: "\uf01e",   #thunderstorm
-    300: "\uf01c",   #sprinkle
-    301: "\uf01c",   #sprinkle
-    302: "\uf019",   #rain
-    310: "\uf017",   #rain-mix
-    311: "\uf019",   #rain
-    312: "\uf019",   #rain
-    313: "\uf01a",   #showers
-    314: "\uf019",   #rain
-    321: "\uf01c",   #sprinkle
-    500: "\uf019",   #light rain
-    501: "\uf019",   #moderate rain
-    502: "\uf015",   #heavy intense rain
-    503: "\uf018",   #very heavy rain
-    504: "\uf018",   #extreme rain
-    511: "\uf017",   #freezing rain
-    520: "\uf01c",   #light intensity showers
-    521: "\uf01a",   #rain showers
-    522: "\uf015",   #heavy rain showers
-    531: "\uf01d",   #storm-showers
-    600: "\uf01b",   #light snow
-    601: "\uf01b",   #snow
-    602: "\uf064",   #heavy snow
-    611: "\uf0b5",   #sleet
-    612: "\uf017",   #light shower sleet
-    612: "\uf017",   #shower sleet
-    615: "\uf017",   #light rain and snow
-    616: "\uf017",   #rain and snow
-    620: "\uf01b",   #light shower snow
-    621: "\uf01b",   #shower snow
-    622: "\uf064",   #heavy shower snow
-    701: "\uf014",   #mist
-    711: "\uf062",   #smoke
-    721: "\uf063",   #haze
-    731: "\uf063",   #dust
-    741: "\uf021",   #fog
-    751: "\uf063",   #sand
-    761: "\uf063",   #dust
-    762: "\uf063",   #dust
-    771: "\uf014",   #squalls
-    781: "\uf056",   #tornado
-    800: "\uf00d",   #sunny
-    801: "\uf041",   #cloud
-    802: "\uf00c",   #clouds  25-50%
-    803: "\uf002",   #clouds  51-84%
-    804: "\uf013",   #clouds  85-100%
-    900: "\uf056",   #tornado
-    901: "\uf01d",   #storm-showers
-    902: "\uf073",   #hurricane
-    903: "\uf076",   #snowflake-cold
-    904: "\uf055",   #hot
-    905: "\uf021",   #windy
-    906: "\uf015",   #hail
-    957: "\uf050"    #strong-wind
+  ID_MAP1 = {                   #id: (day,night)
+    200: ("\uf01e","\uf01e"),   #thunderstorm
+    201: ("\uf01e","\uf01e"),   #thunderstorm
+    202: ("\uf01e","\uf01e"),   #thunderstorm
+    210: ("\uf016","\uf016"),   #lightning
+    211: ("\uf016","\uf016"),   #lightning
+    212: ("\uf016","\uf016"),   #lightning
+    221: ("\uf016","\uf016"),   #lightning
+    230: ("\uf01e","\uf01e"),   #thunderstorm
+    231: ("\uf01e","\uf01e"),   #thunderstorm
+    232: ("\uf01e","\uf01e"),   #thunderstorm
+    300: ("\uf01c","\uf01c"),   #sprinkle
+    301: ("\uf01c","\uf01c"),   #sprinkle
+    302: ("\uf019","\uf019"),   #rain
+    310: ("\uf017","\uf017"),   #rain-mix
+    311: ("\uf019","\uf019"),   #rain
+    312: ("\uf019","\uf019"),   #rain
+    313: ("\uf01a","\uf01a"),   #showers
+    314: ("\uf019","\uf019"),   #rain
+    321: ("\uf01c","\uf01c"),   #sprinkle
+    500: ("\uf019","\uf019"),   #light rain
+    501: ("\uf019","\uf019"),   #moderate rain
+    502: ("\uf015","\uf015"),   #heavy intense rain
+    503: ("\uf018","\uf018"),   #very heavy rain
+    504: ("\uf018","\uf018"),   #extreme rain
+    511: ("\uf017","\uf017"),   #freezing rain
+    520: ("\uf01c","\uf01c"),   #light intensity showers
+    521: ("\uf01a","\uf01a"),   #rain showers
+    522: ("\uf015","\uf015"),   #heavy rain showers
+    531: ("\uf01d","\uf01d"),   #storm-showers
+    600: ("\uf01b","\uf01b"),   #light snow
+    601: ("\uf01b","\uf01b"),   #snow
+    602: ("\uf064","\uf064"),   #heavy snow
+    611: ("\uf0b5","\uf0b5"),   #sleet
+    612: ("\uf017","\uf017"),   #light shower sleet
+    612: ("\uf017","\uf017"),   #shower sleet
+    615: ("\uf017","\uf017"),   #light rain and snow
+    616: ("\uf017","\uf017"),   #rain and snow
+    620: ("\uf01b","\uf01b"),   #light shower snow
+    621: ("\uf01b","\uf01b"),   #shower snow
+    622: ("\uf064","\uf064"),   #heavy shower snow
+    701: ("\uf014","\uf014"),   #mist
+    711: ("\uf062","\uf062"),   #smoke
+    721: ("\uf063","\uf063"),   #haze
+    731: ("\uf063","\uf063"),   #dust
+    741: ("\uf021","\uf021"),   #fog
+    751: ("\uf063","\uf063"),   #sand
+    761: ("\uf063","\uf063"),   #dust
+    762: ("\uf063","\uf063"),   #dust
+    771: ("\uf014","\uf014"),   #squalls
+    781: ("\uf056","\uf056"),   #tornado
+    800: ("\uf00d","\uf00d"),   #sunny
+    801: ("\uf041","\uf041"),   #cloud
+    802: ("\uf00c","\uf00c"),   #clouds  25-50%
+    803: ("\uf002","\uf002"),   #clouds  51-84%
+    804: ("\uf013","\uf013"),   #clouds  85-100%
+    900: ("\uf056","\uf056"),   #tornado
+    901: ("\uf01d","\uf01d"),   #storm-showers
+    902: ("\uf073","\uf073"),   #hurricane
+    903: ("\uf076","\uf076"),   #snowflake-cold
+    904: ("\uf055","\uf055"),   #hot
+    905: ("\uf021","\uf021"),   #windy
+    906: ("\uf015","\uf015"),   #hail
+    957: ("\uf050" "\uf050")    #strong-wind
     }
   ID_MAP2 = {
-    '2': "\uf01e",   #thunderstorm
-    '3': "\uf019",   #rain
-    '5': "\uf019",   #rain
-    '6': "\uf01b"    #snow
+    '2': ("\uf01e","\uf01e"),   #thunderstorm
+    '3': ("\uf019","\uf019"),   #rain
+    '5': ("\uf019","\uf019"),   #rain
+    '6': ("\uf01b" "\uf01b")    #snow
     }
   DIR_MAP = {
     "N":  "\uF060",
@@ -145,11 +145,11 @@ class WeatherContentProvider(ContentProvider):
 
     # try specific code first
     if id in WeatherContentProvider.ID_MAP1:
-      return WeatherContentProvider.ID_MAP1[id]
+      return WeatherContentProvider.ID_MAP1[id][0]
     # try generic code
     code = str(id)[0]
     if code in WeatherContentProvider.ID_MAP2:
-      return WeatherContentProvider.ID_MAP2[code]
+      return WeatherContentProvider.ID_MAP2[code][0]
     # otherwise return default ("n/a")
       return "\uf07b"
 
